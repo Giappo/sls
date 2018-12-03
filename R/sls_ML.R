bed56944e62c04c9791bb752e80d469ce5ed24da
Giappo <glaudanno@gmail.com>
2018-12-03 17:41:12 +0100

    # nolint internal function

--- R/sls_ML.R
+++ R/sls_ML.R
@@ -31,7 +31,7 @@ sls_ml <- function(
   # define function to optimize
   optim_fun <- function(tr_optim_pars) {
     pars2 <- rep(0, length(start_pars))
-    optim_pars <- pars_transform_back(tr_optim_pars)
+    optim_pars <- pars_transform_back(tr_optim_pars) # nolint internal function
     pars2[optim_ids] <- optim_pars
     pars2[!optim_ids] <- true_pars[!optim_ids]
 
