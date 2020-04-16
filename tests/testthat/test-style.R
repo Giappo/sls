732b3811fa66c166e1956821c1c27919e99b3a02
Giappo <glaudanno@gmail.com>
2020-04-16 16:03:55 +0200

    right test-style

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -2,6 +2,8 @@ context("package style")
 
 test_that("package style", {
   lintr::expect_lint_free(
-    linters = lintr::cyclocomp_linter(complexity_limit = 30)
+    linters = lintr::with_defaults(
+      cyclocomp_linter = lintr::cyclocomp_linter(complexity_limit = 30)
+    )
   )
 })
