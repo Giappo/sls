e4b7ca3a1307ec518399326e08a81e072054a7f9
Giappo <glaudanno@gmail.com>
2021-01-21 15:13:42 +0100

    slight change

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -1,9 +1,17 @@
 context("package style")
 
+is_on_ci <- function() {
+  is_it_on_appveyor <- Sys.getenv("APPVEYOR") != ""
+  is_it_on_travis <- Sys.getenv("TRAVIS") != ""
+  is_it_on_appveyor || is_it_on_travis # nolint internal function
+}
+
 test_that("package style", {
-  lintr::expect_lint_free(
-    linters = lintr::with_defaults(
-      cyclocomp_linter = lintr::cyclocomp_linter(complexity_limit = 30)
+  if (Sys.info()['sysname'] != "Windows" && is_on_ci()) {
+    lintr::expect_lint_free(
+      linters = lintr::with_defaults(
+        cyclocomp_linter = lintr::cyclocomp_linter(complexity_limit = 30)
+      )
     )
-  )
+  }
 })
